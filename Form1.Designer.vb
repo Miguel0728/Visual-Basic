ce4ba83cc1dbd2f3668f6b950a17a3ee363aa122
Miguel0728 <melendezmiguel13@gmail.com>
2024-03-13 11:29:34 -0400

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtNota4 = New System.Windows.Forms.TextBox()
+        Me.txtNota3 = New System.Windows.Forms.TextBox()
+        Me.txtNota2 = New System.Windows.Forms.TextBox()
+        Me.txtNota1 = New System.Windows.Forms.TextBox()
+        Me.txtNumEstudiante = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblnota = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.lblMensaje = New System.Windows.Forms.Label()
+        Me.lblNotaFinal = New System.Windows.Forms.Label()
+        Me.lblPromedio = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.btnSalir)
+        Me.GroupBox1.Controls.Add(Me.btnBorrar)
+        Me.GroupBox1.Controls.Add(Me.btnCalcular)
+        Me.GroupBox1.Controls.Add(Me.txtNota4)
+        Me.GroupBox1.Controls.Add(Me.txtNota3)
+        Me.GroupBox1.Controls.Add(Me.txtNota2)
+        Me.GroupBox1.Controls.Add(Me.txtNota1)
+        Me.GroupBox1.Controls.Add(Me.txtNumEstudiante)
+        Me.GroupBox1.Controls.Add(Me.txtApellido)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(27, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(626, 352)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Notas_Estudiantiles"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(378, 299)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(86, 31)
+        Me.btnSalir.TabIndex = 18
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(242, 299)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(91, 31)
+        Me.btnBorrar.TabIndex = 17
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(99, 299)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(106, 31)
+        Me.btnCalcular.TabIndex = 16
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtNota4
+        '
+        Me.txtNota4.Location = New System.Drawing.Point(549, 214)
+        Me.txtNota4.Name = "txtNota4"
+        Me.txtNota4.Size = New System.Drawing.Size(61, 27)
+        Me.txtNota4.TabIndex = 15
+        '
+        'txtNota3
+        '
+        Me.txtNota3.Location = New System.Drawing.Point(392, 218)
+        Me.txtNota3.Name = "txtNota3"
+        Me.txtNota3.Size = New System.Drawing.Size(61, 27)
+        Me.txtNota3.TabIndex = 14
+        '
+        'txtNota2
+        '
+        Me.txtNota2.Location = New System.Drawing.Point(242, 218)
+        Me.txtNota2.Name = "txtNota2"
+        Me.txtNota2.Size = New System.Drawing.Size(61, 27)
+        Me.txtNota2.TabIndex = 13
+        '
+        'txtNota1
+        '
+        Me.txtNota1.Location = New System.Drawing.Point(99, 218)
+        Me.txtNota1.Name = "txtNota1"
+        Me.txtNota1.Size = New System.Drawing.Size(61, 27)
+        Me.txtNota1.TabIndex = 12
+        '
+        'txtNumEstudiante
+        '
+        Me.txtNumEstudiante.Location = New System.Drawing.Point(242, 135)
+        Me.txtNumEstudiante.Name = "txtNumEstudiante"
+        Me.txtNumEstudiante.Size = New System.Drawing.Size(234, 27)
+        Me.txtNumEstudiante.TabIndex = 11
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(242, 92)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(234, 27)
+        Me.txtApellido.TabIndex = 10
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(242, 47)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(234, 27)
+        Me.txtName.TabIndex = 9
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(178, 57)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 20)
+        Me.Label8.TabIndex = 7
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(478, 221)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Nota4"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(321, 221)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Nota3"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(162, 221)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Nota2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 221)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nota1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(80, 142)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(147, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Num.Estudiante:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(80, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Apellidos:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(80, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox2.Controls.Add(Me.lblnota)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.lblMensaje)
+        Me.GroupBox2.Controls.Add(Me.lblNotaFinal)
+        Me.GroupBox2.Controls.Add(Me.lblPromedio)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(659, 45)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(408, 279)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados"
+        '
+        'lblnota
+        '
+        Me.lblnota.BackColor = System.Drawing.Color.White
+        Me.lblnota.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblnota.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnota.Location = New System.Drawing.Point(174, 148)
+        Me.lblnota.Name = "lblnota"
+        Me.lblnota.Size = New System.Drawing.Size(105, 20)
+        Me.lblnota.TabIndex = 11
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(53, 148)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(48, 20)
+        Me.Label12.TabIndex = 10
+        Me.Label12.Text = "Nota"
+        '
+        'lblMensaje
+        '
+        Me.lblMensaje.BackColor = System.Drawing.Color.White
+        Me.lblMensaje.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMensaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMensaje.Location = New System.Drawing.Point(174, 184)
+        Me.lblMensaje.Name = "lblMensaje"
+        Me.lblMensaje.Size = New System.Drawing.Size(192, 33)
+        Me.lblMensaje.TabIndex = 9
+        '
+        'lblNotaFinal
+        '
+        Me.lblNotaFinal.BackColor = System.Drawing.Color.White
+        Me.lblNotaFinal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNotaFinal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNotaFinal.Location = New System.Drawing.Point(174, 103)
+        Me.lblNotaFinal.Name = "lblNotaFinal"
+        Me.lblNotaFinal.Size = New System.Drawing.Size(105, 30)
+        Me.lblNotaFinal.TabIndex = 8
+        '
+        'lblPromedio
+        '
+        Me.lblPromedio.BackColor = System.Drawing.Color.White
+        Me.lblPromedio.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPromedio.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPromedio.Location = New System.Drawing.Point(174, 60)
+        Me.lblPromedio.Name = "lblPromedio"
+        Me.lblPromedio.Size = New System.Drawing.Size(105, 29)
+        Me.lblPromedio.TabIndex = 7
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(53, 185)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(79, 20)
+        Me.Label11.TabIndex = 6
+        Me.Label11.Text = "Mensaje"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(53, 103)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(95, 20)
+        Me.Label10.TabIndex = 5
+        Me.Label10.Text = "Nota Final"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(53, 60)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(88, 20)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Promedio"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1322, 511)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtNota1 As TextBox
+    Friend WithEvents txtNumEstudiante As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtNota3 As TextBox
+    Friend WithEvents txtNota2 As TextBox
+    Friend WithEvents txtNota4 As TextBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblMensaje As Label
+    Friend WithEvents lblNotaFinal As Label
+    Friend WithEvents lblPromedio As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblnota As Label
+    Friend WithEvents Label12 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Form1
+    'Miguel A. Melendez Hernandez
+    'Y00598196
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        ' VARIABLES
+
+        Dim nota1, nota2, nota3, nota4, promedio As Double
+        Dim NotaFinal As Double
+        Dim Mensaje As String
+        Dim Nota As String
+
+        ' VALIDACION DE DATOS
+        Try
+            nota1 = txtNota1.Text
+            nota2 = txtNota2.Text
+            nota3 = txtNota3.Text
+            nota4 = txtNota4.Text
+
+        Catch ex As Exception
+
+            MessageBox.Show("Please enter a number")
+
+
+        End Try
+        promedio = (nota1 + nota2 + nota3 + nota4) / 100
+        NotaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+        ' CASE DE NOTA FINAL
+
+        Select Case NotaFinal
+
+            Case 95 To 100
+                Nota = "A"
+                Mensaje = "APROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+
+            Case 90 To 94
+                Nota = "A"
+                Mensaje = "DESAPROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+
+            Case 80 To 89
+                Nota = "B"
+                Mensaje = "DESAPROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+            Case 70 To 79
+                Nota = "C"
+                Mensaje = "DESAPROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+            Case 60 To 69
+                Nota = "D"
+                Mensaje = "DESAPROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+            Case 0 To 59
+                Nota = "F"
+                Mensaje = "DESAPROBADO!"
+                lblPromedio.Text = promedio
+                lblMensaje.Text = Mensaje
+                lblNotaFinal.Text = NotaFinal
+                lblnota.Text = Nota
+            Case Else
+                MessageBox.Show("Invalid Score")
+        End Select
+
+
+    End Sub
+
+    'BOTON DE CLEAR
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        txtNota1.Clear()
+        txtNota2.Clear()
+        txtNota3.Clear()
+        txtNota4.Clear()
+        txtName.Clear()
+        txtApellido.Clear()
+        txtNumEstudiante.Clear()
+        lblMensaje.Text = ""
+        lblNotaFinal.Text = ""
+        lblPromedio.Text = ""
+        lblnota.Text = ""
+    End Sub
+
+    'BOTON SALIR
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
+    End Sub
+
+
+End Class
